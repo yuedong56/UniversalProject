3fd00fa2395edcc29146b7ba47e267e48a06885e
yuedong56 <865812052@126.com>
2014-06-09 18:03:52 +0800

    1. 增加网络请求类post请求方法。

--- LYUniversalProject/UniversalProject/Classes/Net/NetWorkRequest.m
+++ LYUniversalProject/UniversalProject/Classes/Net/NetWorkRequest.m
@@ -10,6 +10,7 @@
 
 @implementation NetWorkRequest
 
+#pragma mark - 私有公共方法
 /**
  * @brief 检查网络，若网络不可用则弹出提示
  */
@@ -17,7 +18,7 @@
 {
     if (![LYUtils checkNetworkAvailable]) {
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:nil
-                                                            message:@"当前网络不可用！"
+                                                            message:NetUnavailablePrompt
                                                            delegate:nil
                                                   cancelButtonTitle:nil
                                                   otherButtonTitles:@"确定", nil];
@@ -35,6 +36,11 @@
                     requestKey:(NSString *)key
                          block:(void(^)(NSDictionary *jsonDic, NSError *error))block
 {
+    if (![self checkNetWorkStateAndShowAlertView])
+    {
+        return;
+    }
+    
     //先utf8转一下，以防urlStr里含有汉字
     urlStr = [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     NSURL *url = [NSURL URLWithString:urlStr];
@@ -44,10 +50,7 @@
         if ([request error]) {
             block(nil,[request error]);
         } else {
-            NSData *jsonData = [[request responseString] dataUsingEncoding:NSUTF8StringEncoding];
-            NSError *error = nil;
-            NSDictionary *jsonDic = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
-            block(jsonDic,nil);
+            block([self toDictionaryWithJsonString:[request responseString]],nil);
         }
     }];
     
@@ -56,6 +59,70 @@
     }];
     
     [request startAsynchronous];
+}
+
+/**
+ * @brief 启动一个POST请求
+ */
++ (void)startPostRequestWithURL:(NSString *)urlStr
+                     parameters:(NSDictionary *)parameters
+                     requestKey:(NSString *)key
+                          block:(void(^)(NSDictionary *jsonDic, NSError *error))block
+{
+    if (![self checkNetWorkStateAndShowAlertView])
+    {
+        return;
+    }
+    
+    //先utf8转一下，以防urlStr里含有汉字
+    urlStr = [urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    
+    __unsafe_unretained __block ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:url];
+    [request setRequestMethod:@"POST"];
+    
+    NSString *body = [self toJsonStringWithData:parameters];
+    
+    [request setPostValue:body forKey:key?key:@"data"];
+    
+    [request setCompletionBlock:^{
+        if ([request error]) {
+            block(nil,[request error]);
+        } else {
+            block([self toDictionaryWithJsonString:[request responseString]],nil);
+        }
+    }];
+    
+    [request setFailedBlock:^{
+        block(nil,[request error]);
+    }];
+    
+    [request startAsynchronous];
+}
+
+/**
+ * @brief 将json字符串转为字典(NSDictionary)
+ */
++ (NSDictionary *)toDictionaryWithJsonString:(NSString *)jsonStr
+{
+    NSData *jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error = nil;
+    NSDictionary *jsonDic = [NSJSONSerialization JSONObjectWithData:jsonData options:kNilOptions error:&error];
+    return jsonDic;
+}
+
+/**
+ * @brief 将id类型(如NSArray、NSDictionary)转为json字符串
+ */
++ (NSString *)toJsonStringWithData:(id)data
+{
+    NSError *error = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:data options:NSJSONWritingPrettyPrinted error:&error];
+    if ([jsonData length] > 0 && error == nil){
+        return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }else{
+        return nil;
+    }
 }
 
 @end
