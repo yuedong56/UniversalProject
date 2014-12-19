44c5e3321a2f89985ae04664974796d7bc6901f5
yuedong56 <865812052@126.com>
2014-12-19 18:18:48 +0800

    1.增加几个宏定义。

--- LYUniversalProject/UniversalProject/Classes/Public/GlobalConfig.h
+++ LYUniversalProject/UniversalProject/Classes/Public/GlobalConfig.h
@@ -22,7 +22,7 @@
 #define NetUnavailablePrompt @"当前网络不可用！"
 
 //应用的AppID
-#define AppID @"915711237"
+#define AppID @""
 
 //打印
 #ifdef DEBUG
@@ -51,14 +51,33 @@
 #define GrayColor [UIColor grayColor]                //灰色
 #define LightGrayColor [UIColor lightGrayColor]      //浅灰色
 #define DarkGrayColor [UIColor darkGrayColor]        //深灰色
-#define RedColor [UIColor redColor]
+#define RedColor [UIColor redColor]                  //红色
+
+//字号
+#define FONT(X) [UIFont systemFontOfSize:X]
+#define FONT_BOLD(X) [UIFont boldSystemFontOfSize:X]
 
 //16进制的颜色
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
-
 
 //图片
 #define ImageWithFile(name) ([UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:name ofType:nil]])
 #define ImageNamed(name) [UIImage imageNamed:name]
 
+//关于坐标
+#define  Center_x(w) (ScreenWidth-w)/2.0  //控件宽度w，控件水平居中位置的x
+#define  Center_y(h, super_h)  (super_h-h)/2  //控件高度，控件垂直居位置的y
+
+//应用版本
+#define  App_Version  [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
+
+//系统版本
+#define  IOS_Version  [NSString stringWithFormat:@"%@",[[UIDevice currentDevice] systemVersion]]
+
 #endif
+
+
+
+
+
+
